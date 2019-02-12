4c72bda37f87b5e9cc9bc13b070460827ece0266
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-11 21:48:32 -0800

    Add libkeccak library files

---
+++ libkeccak/libkeccak.h
@@ -0,0 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef LIBKECCAK_H
+#define LIBKECCAK_H 1
+
+#include "libkeccak/spec.h"
+#include "libkeccak/generalised-spec.h"
+#include "libkeccak/state.h"
+#include "libkeccak/digest.h"
+#include "libkeccak/hex.h"
+#include "libkeccak/files.h"
+#include "libkeccak/mac/hmac.h"
+
+#endif
