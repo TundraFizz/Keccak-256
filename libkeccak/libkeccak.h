97286e8533179694040821b6943c94f31e986e07
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-12 19:11:37 -0800

    Update libkeccak.h

--- libkeccak/libkeccak.h
+++ libkeccak/libkeccak.h
@@ -1,6 +1,5 @@
-/* See LICENSE file for copyright and license details. */
 #ifndef LIBKECCAK_H
-#define LIBKECCAK_H 1
+#define LIBKECCAK_H
 
 #include "libkeccak/spec.h"
 #include "libkeccak/generalised-spec.h"
