df1b4c93fe9d17f497354ee7e2d81ff8dca8e0bf
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-13 23:09:08 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 
-#ifdef __cplusplus
-#   ifdef __GNUC__
-#       define restrict __restrict__ // G++ has restrict
-#   else
-#       define restrict // C++ in general doesn't
-#   endif
-#endif
+#define restrict // C++ doesn't have the restrict keyword
 
 extern "C" {
   #include "keccak256.h"
@@ -15,7 +9,6 @@ extern "C" {
 int main(){
   char* test = run("public.key");
   std::cout << test << "\n";
-  free(test);
 
   return 0;
 }
