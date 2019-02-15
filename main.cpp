f996dff5cfed39106e3027322d8c3636ea6405a0
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-14 23:51:53 -0800

    Update main.cpp with new test data

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 #include <iostream>
 
 int main(void){
-  const char* publicKey = "836b35a026743e823a90a0ee3b91bf615c6a757e2b60b9e1dc1826fd0dd16106f7bc1e8179f665015f43c6c81f39062fc2086ed849625c06e04697698b21855e";
+  // Private Key
+  // abcdef1203405600789001112233aabbcc24680abcdef00001234567890abcde
+
+  const char* publicKey = "64c9992d70d56cf60383b86dcba395ee0ccdb780b13d1b52803b010ae62574b68ebc46f0b25acf3721da182a180b985500669ec8541244752ec1331ea61aacee";
   char* address = PublicKeyToAddress(publicKey);
   std::cout << address << "\n";
+
+  // 3bb89452fe5544e057767a22e7b8a14e8338963e64fb146cd22746b543d339e8
+  //                       0xe7B8a14E8338963E64fB146cd22746B543D339e8
+
   return 0;
 }
