72eac31f5c134667f29c69e01909d4035edb8424
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-14 21:30:44 -0800

    Accommodate for directory changes

--- libkeccak/libkeccak.h
+++ libkeccak/libkeccak.h
@@ -1,12 +1,14 @@
 #ifndef LIBKECCAK_H
 #define LIBKECCAK_H
 
-#include "libkeccak/spec.h"
+// extern "C" {
+  #include "libkeccak/spec.h"
+// }
 #include "libkeccak/generalised-spec.h"
 #include "libkeccak/state.h"
 #include "libkeccak/digest.h"
 #include "libkeccak/hex.h"
 #include "libkeccak/files.h"
-#include "libkeccak/mac/hmac.h"
+#include "libkeccak/hmac.h"
 
 #endif
