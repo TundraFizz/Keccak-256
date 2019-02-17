a7d7cc83f531516e1769fe2af354bfa7f43f585f
Tundra Fizz <MageLeif@Yahoo.com>
2019-02-16 23:12:34 -0800

    Update test files

--- test.cpp
+++ test.cpp
@@ -49,8 +49,8 @@ int main(int argc, char *argv[]){
   std::cout << "DURATION IN SECONDS: " << SHORTEN(DIFFERENCE(t1, t2)) << "\n";
 
   for(int i = 0; i < keysToGenerate; ++i){
-    // delete[] keyring[i];
-    free(keyring[i]);
+    delete[] keyring[i];
+    // free(keyring[i]);
   }
 
   delete[] keyring;
@@ -60,6 +60,7 @@ int main(int argc, char *argv[]){
   const char* publicKeySingle = "64c9992d70d56cf60383b86dcba395ee0ccdb780b13d1b52803b010ae62574b68ebc46f0b25acf3721da182a180b985500669ec8541244752ec1331ea61aacee";
   char* address = PublicKeyToAddress(publicKeySingle);
   std::cout << "# " << address << "\n";
+  delete[] address;
   // 3bb89452fe5544e057767a22e7b8a14e8338963e64fb146cd22746b543d339e8
   //                       0xe7B8a14E8338963E64fB146cd22746B543D339e8
 
